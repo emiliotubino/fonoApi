e4c996d0612720239a6ccebd9791a330f4ccd46e
Roberto Tubino <robertotubino@Robertos-MacBook-Pro.local>
2025-09-25 14:21:32 -0300

    auth route process

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,16 @@
+import { authMiddleware, AuthRequest } from '../middleware/authMiddleware';
+import { Router, Request, Response } from 'express';
+import Users from '../models/users';
+
+const router = Router();
+
+router.get('/me', authMiddleware, async (req: AuthRequest, res: Response) => {
+  try {
+    const user = await Users.findById(req.user.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar usuário', error });
+  }
+});
+
+export default router;
